03decef1630eafbe740553b60ee41f1304fdb258
Francisco Ponce <francisco.ponce.dev@gmail.com>
2022-05-03 16:18:09 -0300

    create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,11 @@ CREATE TABLE patients (
   name VARCHAR(50),
   date_of_birth DATE
 );
+
+CREATE TABLE medical_histories (
+  id INT PRIMARY KEY,
+  admitted_at timestamp,
+  patient_id INT,
+  FOREIGN KEY (patient_id) REFERENCES patients(id),
+  status VARCHAR(50)
+);
